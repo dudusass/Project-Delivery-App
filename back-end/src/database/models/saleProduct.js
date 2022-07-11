f70b9e73bd1e0a3adace1544651a3c8e2718b4b9
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-11 18:57:12 -0300

    style: change single quote

--- back-end/src/database/models/saleProduct.js
+++ back-end/src/database/models/saleProduct.js
@@ -1,18 +1,25 @@
 module.exports = (sequelize, DataTypes) => {
-  const SaleProduct = sequelize.define('SaleProduct', {
-    saleId: DataTypes.INTEGER,
-    productId: DataTypes.INTEGER,
-    quantity: DataTypes.INTEGER,
-  }, {
-    timestamp: false,
-    underscored: true,
-    table: 'sales'
-  });
+  const SaleProduct = sequelize.define(
+    "SaleProduct",
+    {
+      saleId: DataTypes.INTEGER,
+      productId: DataTypes.INTEGER,
+      quantity: DataTypes.INTEGER,
+    },
+    {
+      timestamp: false,
+      underscored: true,
+      table: "sales_products",
+    }
+  );
 
   SaleProduct.associate = (models) => {
-    SaleProduct.belongsTo(models.Sale, { as: 'sales', foreignKey: 'saleId'});
-    SaleProduct.belongsTo(models.Product, { as: 'product', foreignKey: 'productId'});
-  }
+    SaleProduct.belongsTo(models.Sale, { as: "sales", foreignKey: "saleId" });
+    SaleProduct.belongsTo(models.Product, {
+      as: "product",
+      foreignKey: "productId",
+    });
+  };
 
   return SaleProduct;
-}
+};
