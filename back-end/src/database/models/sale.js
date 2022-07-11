9f7cebd7aaaa581147d4364ef20d3d6788b8cdf1
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-11 18:55:09 -0300

    fix: change type of saleDate

--- back-end/src/database/models/sale.js
+++ back-end/src/database/models/sale.js
@@ -1,23 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
-  const Sale = sequelize.define('Sale', {
-    userId: DataTypes.INTEGER,
-    sellerId: DataTypes.INTEGER,
-    totalPrice: DataTypes.DECIMAL(4,2),
-    deliveryAddress: DataTypes.STRING,
-    deliveryNumber: DataTypes.STRING,
-    saleDate: DataTypes.DATETIME,
-    status: DataTypes.STRING,
-  }, {
-    timestamp: false,
-    underscored: true,
-    table: 'sales'
-  });
+  const Sale = sequelize.define(
+    "Sale",
+    {
+      userId: DataTypes.INTEGER,
+      sellerId: DataTypes.INTEGER,
+      totalPrice: DataTypes.DECIMAL(4, 2),
+      deliveryAddress: DataTypes.STRING,
+      deliveryNumber: DataTypes.STRING,
+      saleDate: DataTypes.DATE,
+      status: DataTypes.STRING,
+    },
+    {
+      timestamp: false,
+      underscored: true,
+      table: "sales",
+    }
+  );
 
   Sale.associate = (models) => {
-    Sale.belongsTo(models.User, { as: 'user', foreignKey: 'userId'});
-    Sale.belongsTo(models.User, { as: 'user', foreignKey: 'sellerId'});
-    Sale.hasMany(models.SaleProduct, { as: 'saleProduct', foreignKey: 'saleId'});
-  }
+    Sale.belongsTo(models.User, { as: "user", foreignKey: "userId" });
+    Sale.belongsTo(models.User, { as: "user", foreignKey: "sellerId" });
+    Sale.hasMany(models.SaleProduct, {
+      as: "saleProduct",
+      foreignKey: "saleId",
+    });
+  };
 
   return Sale;
-}
+};
