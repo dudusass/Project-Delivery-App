f9fcccd5d4d8bea8677d76af9cca55e261bd8624
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-11 18:51:49 -0300

    feat: create migration sales

---
+++ back-end/src/database/migrations/20220711210247-sales.js
@@ -0,0 +1,53 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("sales", {
+      id: {
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      seller_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      total_price: {
+        type: Sequelize.DECIMAL(4, 2),
+        allowNull: false,
+      },
+      delivery_address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      delivery_number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      sale_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
