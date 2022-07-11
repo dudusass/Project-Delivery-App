2bcce2b16cd9a29b6f720a755bb4e95ccb1500f5
alexdemenezes <alexmenezes.business@outlook.com>
2022-07-11 17:47:48 -0300

    feat: create saleProduct model

---
+++ back-end/src/database/models/saleProduct.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const SaleProduct = sequelize.define('SaleProduct', {
+    saleId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER,
+  }, {
+    timestamp: false,
+    underscored: true,
+    table: 'sales'
+  });
+
+  SaleProduct.associate = (models) => {
+    SaleProduct.belongsTo(models.Sale, { as: 'sales', foreignKey: 'saleId'});
+    SaleProduct.belongsTo(models.Product, { as: 'product', foreignKey: 'productId'});
+  }
+
+  return SaleProduct;
+}
