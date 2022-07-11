70b50c443f41480ad2e8ce5e13530023a9c1f3ff
alexdemenezes <alexmenezes.business@outlook.com>
2022-07-11 17:46:19 -0300

    feat: create user model

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.INTEGER,
+  }, {
+    timestamp: false,
+    underscored: false,
+    table: 'users'
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.Sale, { as: 'sales', foreignKey: 'userId'});
+    User.hasMany(models.Sale, { as: 'sales', foreignKey: 'sellerId'});
+  }
+
+  return User;
+}
+
