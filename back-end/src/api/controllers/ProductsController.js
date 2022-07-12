a3aa7f79934d68695250a24fb474990100bfe0e9
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-12 20:57:52 -0300

    feat: create the class ProductsController

---
+++ back-end/src/api/controllers/ProductsController.js
@@ -0,0 +1,17 @@
+const { StatusCodes } = require('http-status-codes');
+const ProductsService = require('../services/ProductsService');
+
+class ProductsController {
+  constructor() {
+    this.productsService = new ProductsService();
+  }
+
+  async getAll(_req, res) {
+    console.log('Olá');
+    const productsFound = await this.productsService.getAll();
+
+    res.status(StatusCodes.OK).json(productsFound);
+  }
+}
+
+module.exports = ProductsController;
