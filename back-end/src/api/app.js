e5eecec128cd4d37b3ec18a4c4b3c4b7089acd2d
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-12 15:34:04 -0300

    feat: use middleware handleErrors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const handleError = require('./middleware/handleError');
 
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(handleError);
 
 module.exports = app;
 
