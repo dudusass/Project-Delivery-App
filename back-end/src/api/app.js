f2fd68ca7e6f41e9b5458e20af7359a0838228e3
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-13 17:28:58 -0300

    feat: use lib cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const handleError = require('./middleware/handleError');
 const usersRouter = require('./routes/userRouter');
 const productsRouter = require('./routes/productsRouter');
 const salesRouter = require('./routes/salesRouter');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
