b07b2e5f0b8322b62626ae15cc36eecb3c25ac56
alexdemenezes <alexmenezes.business@outlook.com>
2022-07-13 09:52:32 -0300

    feat: create getByName and create method on user model

--- back-end/src/api/models/UserModel.js
+++ back-end/src/api/models/UserModel.js
@@ -14,6 +14,36 @@ class UserModel {
 
     return user;
   }
+
+  async getByName(name) {
+    const user = await this.user.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return user;
+  }
+
+  async create(admin, payload) {
+    const { isAdmin } = admin;
+    const { name, email, password, role } = payload;
+
+    if (isAdmin) {
+      await this.user.create({
+        name, 
+        email,
+        password,
+        role,
+      });
+    } else {
+      await this.user.create({
+        name, 
+        email,
+        password,
+      });
+    }
+  }
 }
 
 module.exports = UserModel;
