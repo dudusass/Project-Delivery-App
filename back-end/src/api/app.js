d9bd7630b2366cfaf4ec94dba9c55c075dc7da37
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-12 21:01:57 -0300

    feat: Implements the route static for images, implements the endpoint /products and use express.json

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const handleError = require('./middleware/handleError');
+const productsRouter = require('./routes/productsRouter');
 
 const app = express();
+app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use('/api/products', productsRouter);
 
 app.use(handleError);
 
