68899d67035a95697ff8ac5e64ab8cd1e37943e2
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-13 17:16:40 -0300

    feat: add middleware of authentication and validation for salesRouter

--- back-end/src/api/routes/salesRouter.js
+++ back-end/src/api/routes/salesRouter.js
@@ -1,13 +1,33 @@
 const { Router } = require('express');
 const SalesController = require('../controllers/SalesController');
+const AuthMiddleware = require('../middleware/AuthMiddleware');
+const SalesMiddleware = require('../middleware/SalesMiddleware');
 
 const route = Router();
 
+const auth = new AuthMiddleware();
+const salesMiddleware = new SalesMiddleware();
 const salesController = new SalesController();
 
 route
-  .get('/', (req, res) => salesController.getByUser(req, res))
-  .get('/:id', (req, res) => salesController.getById(req, res))
-  .post('/', (req, res) => salesController.create(req, res));
+  .get(
+    '/',
+    (req, res, next) => auth.verifyToken(req, res, next),
+    (req, res) => salesController.getByUser(req, res),
+  )
+  .get(
+    '/:id',
+    (req, res, next) => auth.verifyToken(req, res, next),
+    (req, res) => salesController.getById(req, res),
+  )
+  .post(
+    '/',
+    (req, res, next) => auth.verifyToken(req, res, next),
+    (req, res, next) => salesMiddleware.verifyTotalPrice(req, res, next),
+    (req, res, next) => salesMiddleware.verifyDeliveryAddress(req, res, next),
+    (req, res, next) => salesMiddleware.verifyDeliveryNumber(req, res, next),
+    (req, res, next) => salesMiddleware.verifySaleProducts(req, res, next),
+    (req, res) => salesController.create(req, res),
+  );
 
 module.exports = route;
