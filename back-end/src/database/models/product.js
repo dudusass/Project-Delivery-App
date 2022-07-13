b1027e121b10e0551c8a723881f19632b498ec88
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-12 21:07:10 -0300

    fix: change product model timestamp for timestamps

--- back-end/src/database/models/product.js
+++ back-end/src/database/models/product.js
@@ -1,17 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
-  const Product = sequelize.define('Product', {
-    name: DataTypes.STRING,
-    price: DataTypes.DECIMAL(4,2),
-    urlImage: DataTypes.STRING,
-  }, {
-    timestamp: false,
-    underscored: true,
-    table: 'products'
-  });
+  const Product = sequelize.define(
+    'Product',
+    {
+      name: DataTypes.STRING,
+      price: DataTypes.DECIMAL(4, 2),
+      urlImage: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      table: 'products',
+    },
+  );
 
   Product.associate = (models) => {
-    Product.hasMany(models.SaleProduct, { as: 'saleProducts', foreignKey: 'productId'});
-  }
+    Product.hasMany(models.SaleProduct, { as: 'saleProducts', foreignKey: 'productId' });
+  };
 
   return Product;
-}
+};
