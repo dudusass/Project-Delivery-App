1966d71cd4fac196f9b8d95ddc619e8c9bf6614e
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-13 00:38:02 -0300

    fix: Changed timestamp to timestamps

--- back-end/src/database/models/user.js
+++ back-end/src/database/models/user.js
@@ -1,20 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.INTEGER,
-  }, {
-    timestamp: false,
-    underscored: false,
-    table: 'users'
-  });
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.INTEGER,
+    },
+    {
+      timestamps: false,
+      underscored: false,
+      table: 'users',
+    },
+  );
 
   User.associate = (models) => {
-    User.hasMany(models.Sale, { as: 'user', foreignKey: 'userId'});
-    User.hasMany(models.Sale, { as: 'seller', foreignKey: 'sellerId'});
-  }
+    User.hasMany(models.Sale, { as: 'user', foreignKey: 'userId' });
+    User.hasMany(models.Sale, { as: 'seller', foreignKey: 'sellerId' });
+  };
 
   return User;
-}
-
+};
