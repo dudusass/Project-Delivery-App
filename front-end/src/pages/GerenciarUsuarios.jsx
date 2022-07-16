843e314a71dd3920eebc90ba7f9fac4a84b586ba
Douglas dos Santos <doug.frutuoso@gmail.com>
2022-07-16 10:41:45 -0300

    arrumando product card

---
+++ front-end/src/pages/GerenciarUsuarios.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function GerenciarUsuarios() {
+  return (
+    <div>
+      <h1> Cadastrar Novo usuário </h1>
+      <h1> Lista de usuários</h1>
+
+    </div>
+  );
+}
+
+export default GerenciarUsuarios;
