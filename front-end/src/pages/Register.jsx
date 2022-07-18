b605688b168f5a001dbc1b395e3cc057d13a2322
Sass <dudu_sass12@hotmail.com>
2022-07-18 16:43:55 -0300

    register page branch correta

---
+++ front-end/src/pages/Register.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import './register.css';
+
+function Register() {
+  const [displayErrorMsg, setDisplayErrorMsg] = useState(false);
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  function handleChange({ target }) {
+    const { name, value } = target;
+    setInputs((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  async function handleClickRegister() {
+    try {
+      const returnRequest = await axios.post('http://localhost:3001/api/users/login', inputs);
+      saveStorage('user', returnRequest.data);
+      navigate('/customer/products');
+    } catch (error) {
+      setDisplayErrorMsg(true);
+    }
+  }
+
+  function enableButton() {
+    const { name, email, password } = inputs;
+    const minName = 12;
+    const minPassword = 6;
+    if (password.length >= minPassword && name.length >= minName) {
+      const regex = /^[a-z0-9_.-]+@[a-z]+\.[a-z]{2,3}(?:\.[a-z]{2})?$/;
+      if (regex.test(email)) return false;
+    }
+    return true;
+  }
+
+  return (
+    <div>
+      <form className="registerForm">
+        <label htmlFor="Nome">
+          <h3>Nome</h3>
+          <input
+            type="text"
+            id="text"
+            name="name"
+            data-testid="common_register__input-name"
+            placeholder="Seu nome"
+            value={ inputs.nome }
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor="email">
+          <h3>Email</h3>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            data-testid="common_register__input-email"
+            placeholder="seu-email@trybeer.com.br"
+            value={ inputs.email }
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          <h3>Senha</h3>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            data-testid="common_register__input-password"
+            placeholder="***********"
+            value={ inputs.password }
+            onChange={ handleChange }
+          />
+        </label>
+        <button
+          className="registerButton"
+          type="submit"
+          disabled={ enableButton() }
+          onClick={ handleClickRegister }
+          data-testid="common_register__button-register"
+        >
+          Cadastrar
+        </button>
+        {
+          displayErrorMsg && (
+            <p data-testid="common_register__element-invalid_register">
+              Usuário já cadastrado !
+            </p>
+          )
+        }
+      </form>
+    </div>
+  );
+}
+
+export default Register;
