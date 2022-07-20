3a0b887e857d46b4c8be2248a4b9466f6eba71ac
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-07-20 16:34:26 -0300

    fix: change tableName sales_products for salesProducts

--- back-end/src/database/models/saleProduct.js
+++ back-end/src/database/models/saleProduct.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       timestamps: false,
       underscored: true,
-      tableName: 'sales_products',
+      tableName: 'salesProducts',
     },
   );
 
