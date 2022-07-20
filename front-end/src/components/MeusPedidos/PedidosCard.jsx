19adb45f5294026548d4dc965cf2e2857551d5a3
ProHatp <prohat.0731@gmail.com>
2022-07-20 11:51:12 -0300

    fix outros erros

--- front-end/src/components/MeusPedidos/PedidosCard.jsx
+++ front-end/src/components/MeusPedidos/PedidosCard.jsx
@@ -1,31 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import '../../css/PedidosCard.css';
+import dateFormato from '../../utils/dateFormat';
 
 function PedidosCard(props) {
   const { id, status, saleDate, totalPrice } = props;
 
   return (
-    <div className="pedidosContainer">
-      <div className="pedidos">
-        <p
-          data-testid={ `customer_orders__element-order-id-${id}` }
-        >
-          { id }
-        </p>
+    <Link
+      to={ {
+        pathname: `/customer/orders/${id}`,
+        state: { fromDashboard: true },
+      } }
+    >
+      <div className="pedidosContainer">
+        <div className="pedidos">
+          <p
+            data-testid={ `customer_orders__element-order-id-${id}` }
+          >
+            { id }
+          </p>
+        </div>
+        <div className="status">
+          <p
+            data-testid={ `customer_orders__element-delivery-status-${id}` }
+          >
+            {`${status}`}
+          </p>
+        </div>
+        <div className="data">
+          <p
+            data-testid={ `customer_orders__element-order-date-${id}` }
+          >
+            {`${dateFormato(saleDate)}`}
+          </p>
+          <p
+            data-testid={ `customer_orders__element-card-price-${id}` }
+          >
+            {`R$ ${totalPrice.replace('.', ',')}`}
+          </p>
+        </div>
       </div>
-      <div className="status">
-        <p>{`${status}`}</p>
-      </div>
-      <div className="data">
-        <p data-testid={ `customer_orders__element-order-date-${id}` }>{`${saleDate}`}</p>
-        <p
-          data-testid={ `customer_orders__element-card-price-${id}` }
-        >
-          {`R$ ${totalPrice.replace('.', ',')}`}
-        </p>
-      </div>
-    </div>
+    </Link>
   );
 }
 
